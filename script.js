916ef362b10d7e8eaf0597c4b96ddd0444045146
Rishuraj Singh <102401151+rraawww10@users.noreply.github.com>
2024-12-02 16:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const draggables = document.querySelectorAll('.grid-item');
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging');
+  });
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging');
+  });
+});
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    const draggableElement = document.querySelector('.dragging');
+    const draggableId = draggableElement.id;
+    const droppableId = draggable.id;
+    const draggableImage = document.getElementById(draggableId).style.backgroundImage;
+    const droppableImage = document.getElementById(droppableId).style.backgroundImage;
+    document.getElementById(draggableId).style.backgroundImage = droppableImage;
+    document.getElementById(droppableId).style.backgroundImage = draggableImage;
+  });
+});
